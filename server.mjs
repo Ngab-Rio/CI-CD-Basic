d7b4c386a2660f010287c7d3f141aff2a8bc5f78
Ngab-Rio <mamanadifirmansyah@gmail.com>
2025-07-16 23:36:09 +0700

    Implemented CI/CD

--- server.mjs
+++ server.mjs
@@ -2,6 +2,7 @@ import express from "express";
 
 const app = express();
 const port = 3000;
+const host = 0.0.0.0;
 
 app.get('/', (req, res) => {
   res.send('Welcome to my server, Implemented CI/CD');
@@ -11,6 +12,6 @@ app.get('/halo', (req, res) => {
   res.send('Hello from Express')
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(host, port, () => {
+  console.log(`Server is running on ${host}:${port}`);
 });
